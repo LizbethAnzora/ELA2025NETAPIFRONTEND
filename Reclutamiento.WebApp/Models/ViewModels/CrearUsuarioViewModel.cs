f5e0f5d595e9d2b481801c1a12674fb53d211951
Eneida Anzora <anzoraeneida@outlook.com>
2025-09-22 23:41:09 -0600

    Se creo la clase CrearUsuarioViewModel en la carpeta ViewModels

---
+++ Reclutamiento.WebApp/Models/ViewModels/CrearUsuarioViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ReclutamientoFrontend.WebApp.Models.ViewModels
+{
+    public class CrearUsuarioViewModel
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Debe asignar un rol")]
+        public string? Rol { get; set; } // Admin / Solicitante
+
+        [Required(ErrorMessage = "Debe asignar una contraseña")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
